fb1e2b67345ea99ba74084757a33ed43bb0fa6ea
takahashi <takahashi@jnlp.org>
2016-10-15 13:04:11 +0900

    python-template

---
+++ .emacs.d/template/python-template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+説明
+"""
+
+__author__ = "takahashi <takahashi@jnlp.org>"
+__version__ = "1"
+__date__ = "dd m yyyy"
+
+
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
